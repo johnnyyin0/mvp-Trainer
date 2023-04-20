3a89eaca444ee539c8e4d66baa1f5aa248d5c725
phaeq <johnnyyin0@gmail.com>
2023-04-20 01:51:01 -0400

    Vite config additions

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext',
+    outDir: 'dist',
+    brotliSize: false,
+    sourcemap: true,
+  },
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000,
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
 });
